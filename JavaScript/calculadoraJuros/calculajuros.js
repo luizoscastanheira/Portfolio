cf7a5ccb95c9b10a453d2352833f4038cd7b7e8d
Luiz Otávio - ADS <93355309+luizoscastanheira@users.noreply.github.com>
2023-12-26 11:53:14 -0300

    Calculadora atualizado

--- JavaScript/calculadoraJuros/calculajuros.js
+++ JavaScript/calculadoraJuros/calculajuros.js
@@ -2,34 +2,47 @@
 /* Importando plugin para entrada de ados pelo usuário*/
 import entrada from 'readline-sync';
 
+// Limpando o console antes executar
+console.clear();
+
 console.log("*************************************************************");
 console.log("**                 Calculadora de Juros                    **")
 console.log("*************************************************************");
-
-/* Definindo as variáveis de entrada de dados pelo usuário */
-let valorDivida = entrada.question("Por favor, informe o valor da Dívida: ");
-let diasAtraso = entrada.question("Por favor, informe os dias de atraso: ");
-/* Definindo as taxas de Juros */
-let jurosDez = 10;
-let jurosCinco = 5;
-
-/* Informando ao usário o que ele digitou */
+console.log("*****          Desenvolvida por - Luiz Otávio           *****");
 console.log("*************************************************************");
-console.log("O valor da original da dívida: " + valorDivida);
-console.log("Os dias de atraso: " + diasAtraso);
+/* Definindo as variáveis de entrada de dados pelo usuário */
+let divida = entrada.question("Por favor, informe o valor da Dívida: ");
+let valorDivida = Number(divida);
+let taxaJuros = 0;
 
-/**
- * Aqui vamos implementar os cálculos
-*/
-let valorJuros = (valorDivida / 100) * jurosDez;
-let valorDividaAtualizado = Number(valorDivida) + Number(valorJuros);
-
-
-
-
-
-/* Informe dos juros a serem aplicados */
-console.log("***********        Atualizando Valores       ****************");
-console.log("Taxa de Juros: " + jurosDez + "%.");
-console.log("Valor atualizado da dívida com juros: R$ "+valorDividaAtualizado+".");
-
+if (valorDivida <= 0){
+    console.log("*************************************************************");
+    console.log("*    A Dívida não pode ter valor zero - Fim da Aplicação    *");
+    console.log("*************************************************************");
+}else{
+    console.log("Continuando...");
+    console.log("*************************************************************");
+    let atraso = entrada.question("Informe os dias de atraso: ");
+    let diasAtraso = Number(atraso);
+    if(diasAtraso >=1 && diasAtraso <= 15){
+        taxaJuros = 5;
+        let valorJuros = (valorDivida / 100) * Number(taxaJuros);
+        let valorDividaAtualizado = Number(valorDivida) + valorJuros;
+        console.log("*************************************************************");
+        console.log("Taxa de Juros em 5% devido a "+diasAtraso+" dias de atraso.");
+        console.log("Valor atualizado da dívida é de: R$"+valorDividaAtualizado+".");
+        console.log("*************************************************************");
+    }else if(diasAtraso > 15){
+        taxaJuros = 10;
+        let valorJuros = (valorDivida / 100) * taxaJuros;
+        let valorDividaAtualizado = Number(valorDivida) + Number(valorJuros);
+        console.log("*************************************************************");
+        console.log("Taxa de Juros em 10% devido a "+diasAtraso+" dias de atraso.");
+        console.log("Valor atualizado da dívida é de: R$"+valorDividaAtualizado+".");
+        console.log("*************************************************************");
+    }else{
+        console.log("*************************************************************");
+        console.log("*        Sua dívida não está em atraso - Obrigado!          *");
+        console.log("*************************************************************");
+    }
+}
