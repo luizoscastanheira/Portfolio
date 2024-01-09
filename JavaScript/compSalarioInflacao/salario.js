669429e1c9bfd7dda2cb623a7689ad8a4603d326
Luiz Otávio - ADS <93355309+luizoscastanheira@users.noreply.github.com>
2024-01-09 18:10:28 -0300

    Atualizando
    Inserindo novo projeto JavaScript

---
+++ JavaScript/compSalarioInflacao/salario.js
@@ -0,0 +1,34 @@
+/**
+ * Aplicação para exibir o salário mínimo, inflação do IPCA entres os anos de 
+ * 2010 a 2020, além do percentual de crescimento salarial
+*/
+// Importando o readline-sync para usar nos inputs
+import dados from 'readline-sync';
+
+//limpando o terminal a cada execução
+console.clear();
+
+// Definindo a Tela de Abertura
+console.log("*********************************************************");
+console.log("*                                                       *");
+console.log("*                Salário  X  Inflação                   *");
+console.log("*                                                       *");
+console.log("*********************************************************");
+
+// Definindo a Coleção de Dados a ser usada
+
+
+
+
+
+
+
+// Fim da coleção
+
+
+// Criando um menu de opções
+console.log("Seja bem-vindo ao sistema!");
+console.log("Selecione uma opcão abaixo pelo número correspondente:");
+console.log("*                                                       *");
+console.log("O que você deseja ver?");
+console.log("*                                                       *");
